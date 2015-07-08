e11aa98d447ea7366db5f6f28e8dcb80ef642407
Sarah Gould <sarah.gould@gmail.com>
2015-07-08 15:52:30 -0700

    Chapter 6

--- 06-1 A vector type.js
+++ 06-1 A vector type.js
@@ -1,4 +1,19 @@
+function Vector(x, y) {
+    this.x = x;
+    this.y = y;
+}
 
+Vector.prototype.plus = function(v) {
+    return new Vector(this.x + v.x, this.y + v.y);
+}
+
+Vector.prototype.minus = function(v) {
+    return new Vector(this.x - v.x, this.y - v.y);
+}
+
+Object.defineProperty(Vector.prototype, 'length', {
+    get: function() { return Math.sqrt(this.x*this.x + this.y*this.y); }
+});
 
 console.log(new Vector(1, 2).plus(new Vector(2, 3)));
 // → Vector{x: 3, y: 5}
@@ -10,6 +25,23 @@ console.log(new Vector(3, 4).length);
 /*
 // Book code
 
+function Vector(x, y) {
+  this.x = x;
+  this.y = y;
+}
 
+Vector.prototype.plus = function(other) {
+  return new Vector(this.x + other.x, this.y + other.y);
+};
+
+Vector.prototype.minus = function(other) {
+  return new Vector(this.x - other.x, this.y - other.y);
+};
+
+Object.defineProperty(Vector.prototype, "length", {
+  get: function() {
+    return Math.sqrt(this.x * this.x + this.y * this.y);
+  }
+});
 
 */
