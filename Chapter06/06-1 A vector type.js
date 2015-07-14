130240b083938074c590ceb9bcd0dd94485c9cd9
Sarah Gould <sarah.gould@gmail.com>
2015-07-14 07:29:50 -0700

    Chapter 8

--- Chapter06/06-1 A vector type.js
+++ Chapter06/06-1 A vector type.js
@@ -1,3 +1,33 @@
+// New answer using factory functions.
+var vector = {
+    plus: function (v) {
+        return createVector(this.x + v.x, this.y + v.y);
+    },
+    minus: function (v) {
+        return createVector(this.x - v.x, this.y - v.y);
+    },
+    get length () {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    },
+}
+
+var createVector = function (x, y) {
+    var v = Object.create(vector);
+    v.x = x;
+    v.y = y;
+    return v;
+}
+
+console.log(createVector(1, 2).plus(createVector(2, 3)));
+// → Vector{x: 3, y: 5}
+console.log(createVector(1, 2).minus(createVector(2, 3)));
+// → Vector{x: -1, y: -1}
+console.log(createVector(3, 4).length);
+// → 5
+
+/*
+// Old answer, using constructors.
+
 function Vector(x, y) {
     this.x = x;
     this.y = y;
@@ -14,13 +44,7 @@ Vector.prototype.minus = function(v) {
 Object.defineProperty(Vector.prototype, 'length', {
     get: function() { return Math.sqrt(this.x*this.x + this.y*this.y); }
 });
-
-console.log(new Vector(1, 2).plus(new Vector(2, 3)));
-// → Vector{x: 3, y: 5}
-console.log(new Vector(1, 2).minus(new Vector(2, 3)));
-// → Vector{x: -1, y: -1}
-console.log(new Vector(3, 4).length);
-// → 5
+*/
 
 /*
 // Book code
