71fe20b61c607092c5b72682392ce6934a9a5495
Sarah Gould <sarah.gould@gmail.com>
2015-07-16 18:24:15 -0700

    Chapter 9

---
+++ Chapter09/09-2 Quoting style.js
@@ -0,0 +1,11 @@
+var text = "'I'm the cook,' he said, 'it's my job.'";
+
+var findSingleQuotes = /'(.+?)'(\W|$)/g
+
+function reQuote (match, quote, rest) {
+    return '"' + quote + '"' + rest;
+}
+
+console.log(text.replace(findSingleQuotes, reQuote));
+
+// → "I'm the cook," he said, "it's my job."
